24859940159fc0e2238a4a971f11dde23719bd83
hrmoore927 <hrmoore927@gmail.com>
2018-01-25 22:02:40 -0500

    Added CSC134 chapter exercises folder

---
+++ CSC134/Chap03/helloWorld.cpp
@@ -0,0 +1,17 @@
+//enter your name
+//enter the date here
+//enter identifying information about the assignment
+
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+	cout << "Hello World!" << endl;
+	return 0;
+}
+
+
+
+
